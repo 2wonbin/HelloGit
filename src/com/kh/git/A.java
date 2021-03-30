85d5914f2a5e5a204197c98b9ca7723893a5de02
2wonbin <2wonbin@kakao.com>
2021-03-30 10:42:50 +0900

    delete class B

--- src/com/kh/git/A.java
+++ src/com/kh/git/A.java
@@ -3,6 +3,8 @@ package com.kh.git;
 public class A {
 	
 	public void foo() {
-		
+		System.out.println("foo");
+		B b = new B();
+		b.chu();
 	}
 }
--- src/com/kh/git/B.java
+++ src/com/kh/git/B.java
@@ -1,5 +1,7 @@
 package com.kh.git;
 
 public class B {
-
+	public void chu() {
+		System.out.println("chu~❤");
+	}
 }
